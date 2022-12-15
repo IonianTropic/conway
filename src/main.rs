c7d8c3215b66ab26edcedafae6e3fc536f74d7b5
Io <luke114@outlook.com>
2022-12-14 23:27:09 -0700

    work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 
 
+const SIZE: (usize, usize) = (64, 36);
+
 fn main() {
     
     let event_loop = winit::event_loop::EventLoop::new();
@@ -10,24 +12,51 @@ fn main() {
     
     let surface_texture = pixels::SurfaceTexture::new(initial_size.width, initial_size.height, &window);
 
-    const SIZE: (usize, usize) = (16, 12);
-
     let mut pixels = pixels::Pixels::new(SIZE.0 as u32, SIZE.1 as u32, surface_texture).unwrap();
 
     let mut game_state = [Cell::Dead; SIZE.0*SIZE.1];
 
+    // tub
+    game_state[SIZE.0*1+2] = Cell::Alive;
+    game_state[SIZE.0*2+1] = Cell::Alive;
+    game_state[SIZE.0*2+3] = Cell::Alive;
+    game_state[SIZE.0*3+2] = Cell::Alive;
+
+    // blinker
+    game_state[SIZE.0*2+5] = Cell::Alive;
+    game_state[SIZE.0*3+5] = Cell::Alive;
+    game_state[SIZE.0*4+5] = Cell::Alive;
+
+    let mut paused = true;
+    
     event_loop.run(move |event, _, control_flow| {
-        control_flow.set_wait();
+        control_flow.set_wait_until(std::time::Instant::now().checked_add(std::time::Duration::from_millis(200)).unwrap());
         match event {
             winit::event::Event::WindowEvent { event, .. } => match event {
                 winit::event::WindowEvent::Resized(psize) => {
                     pixels.resize_surface(psize.width, psize.height);
                 }
+                // if P pressed
+                winit::event::WindowEvent::KeyboardInput { 
+                    input:winit::event::KeyboardInput {
+                        state: winit::event::ElementState::Pressed,
+                        virtual_keycode: Some(winit::event::VirtualKeyCode::P), 
+                        ..
+                    },
+                    ..
+                } => {
+                    paused = !paused;
+                }
                 winit::event::WindowEvent::CloseRequested => control_flow.set_exit(),
                 _ => (),
             }
             winit::event::Event::MainEventsCleared => {
-                get_next_state(&mut game_state, SIZE);
+                if !paused {
+                    update_state(&mut game_state);
+                }
+                window.request_redraw();
+            }
+            winit::event::Event::RedrawRequested(_) => {
                 write_game_state(&game_state, &mut pixels, SIZE);
                 pixels.render().unwrap();
             }
@@ -62,6 +91,17 @@ impl Rgba {
     }
 }
 
+fn modulo(a: isize, b: isize) -> isize {
+    let mut r = a;
+    while r < 0 {
+        r += b;
+    }
+    while r >= b {
+        r -= b;
+    }
+    r
+}
+
 fn write_display_test(pixels: &mut pixels::Pixels, pixel_size: (u32, u32)) {
     let frame = pixels.get_frame_mut();
     let mut row = 0;
@@ -81,67 +121,69 @@ fn write_display_test(pixels: &mut pixels::Pixels, pixel_size: (u32, u32)) {
     }
 }
 
-fn get_next_state(game_state: &mut [Cell], size: (usize, usize)) {
-    let mut next_state = Vec::new();
-    let width = size.0 as isize;
-    let height = size.1 as isize;
-    let get_idx = |i: isize, j: isize| ((i % height)*width+(j % height)) as usize;
-    println!("0, 0: {:?}", get_idx(0, 0));
-    println!("1, 1: {:?}", get_idx(1, 1));
-    for i in 0..height {
-        for j in 0..width {
-            let mut count = 0;
-            // check neighborhood
-            // current cell game_state[i*width+j] or game_state[get_idx(i, j)]
-            if game_state[get_idx(i-1,j-1)] == Cell::Alive {
-                count += 1;
-            }
-            if game_state[get_idx(i-1,j)] == Cell::Alive {
-                count += 1;
-            }
-            if game_state[get_idx(i-1,j+1)] == Cell::Alive {
-                count += 1;
-            }
-            if game_state[get_idx(i,j-1)] == Cell::Alive {
-                count += 1;
-            }
-            if game_state[get_idx(i,j+1)] == Cell::Alive {
-                count += 1;
-            }
-            if game_state[get_idx(i+1,j-1)] == Cell::Alive {
-                count += 1;
-            }
-            if game_state[get_idx(i+1,j)] == Cell::Alive {
-                count += 1;
-            }
-            if game_state[get_idx(i+1,j+1)] == Cell::Alive {
-                count += 1;
-            }
-            if game_state[get_idx(i,j)] == Cell::Alive {
-                if count < 2 {
-                    next_state.push(Cell::Dead);
-                } else if count > 3 {
-                    next_state.push(Cell::Dead);
-                } else {
-                    next_state.push(Cell::Alive);
-                }
-            } else {
-                if count == 3 {
-                    next_state.push(Cell::Alive);
-                } else {
-                    next_state.push(Cell::Dead);
-                }
-            }
-        }
-        for i in 0..width*height {
-            game_state[i as usize] = next_state[i as usize];
-        } 
-    }
-}
-
 fn write_game_state(game_state: &[Cell], pixels: &mut pixels::Pixels, size: (usize, usize)) {
     let frame = pixels.get_frame_mut();
     for (pixel, state) in frame.chunks_exact_mut(4).zip(game_state) {
         state.to_rgba().write_to(pixel)
     }
 }
+
+fn update_state(game_state: &mut [Cell]) {
+    let width = SIZE.0 as isize;
+    let height = SIZE.1 as isize;
+    let mut neighboor_count: [u8; SIZE.0*SIZE.1] = [0; SIZE.0*SIZE.1];
+    for row in 0..height {
+        for col in 0..width {
+            let bp = 0;
+            if wrapping_idx(game_state, row-1, col-1) == &Cell::Alive {
+                neighboor_count[SIZE.0*row as usize+col as usize]+=1;
+            }
+            if wrapping_idx(game_state, row-1, col) == &Cell::Alive {
+                neighboor_count[SIZE.0*row as usize+col as usize]+=1;
+            }
+            if wrapping_idx(game_state, row-1, col+1) == &Cell::Alive {
+                neighboor_count[SIZE.0*row as usize+col as usize]+=1;
+            }
+            if wrapping_idx(game_state, row, col-1) == &Cell::Alive {
+                neighboor_count[SIZE.0*row as usize+col as usize]+=1;
+            }
+            if wrapping_idx(game_state, row, col+1) == &Cell::Alive {
+                neighboor_count[SIZE.0*row as usize+col as usize]+=1;
+            }
+            if wrapping_idx(game_state, row+1, col-1) == &Cell::Alive {
+                neighboor_count[SIZE.0*row as usize+col as usize]+=1;
+            }
+            if wrapping_idx(game_state, row+1, col) == &Cell::Alive {
+                neighboor_count[SIZE.0*row as usize+col as usize]+=1;
+            }
+            if wrapping_idx(game_state, row+1, col+1) == &Cell::Alive {
+                neighboor_count[SIZE.0*row as usize+col as usize]+=1;
+            }
+        }
+    }
+    // println!("state: {:?}\nneighboors: {:?}", game_state, neighboor_count);
+    for (cell, neighboors) in game_state.iter_mut().zip(neighboor_count) {
+        match cell {
+            Cell::Alive => if neighboors < 2 || neighboors > 3 {
+                *cell = Cell::Dead;
+            }
+            Cell::Dead => if neighboors == 3 {
+                *cell = Cell::Alive;
+            }
+        }
+    }
+}
+
+fn wrapping_idx(game_state: &[Cell], row: isize, col: isize) -> &Cell {
+    let width = SIZE.0 as isize;
+    let height = SIZE.1 as isize;
+    let idx = (width*modulo(row, height)+modulo(col, width)) as usize;
+    // println!("row: {}\ncol: {}\nidx: {}", row, col, idx);
+    return &game_state[idx]; // -1, 3
+}
+
+// fn mut_wrapping_idx(game_state: &mut [Cell], row: isize, col: isize) -> &mut Cell {
+//     let width = SIZE.0 as isize;
+//     let height = SIZE.1 as isize;
+//     return &mut game_state[(width*(row%height)+(col%width)) as usize];
+// }
